3008cfad2e13ac469b48c47e34e3476c4acdc33f
Krishnanu Singh <140878834+CARBON03@users.noreply.github.com>
2025-03-07 14:28:47 +0530

    Basic Server

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send("<h1>hello</h1>");
+    res.render('index.ejs');
   });
 
 app.listen(port, () => {
